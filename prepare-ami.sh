82053307b7434cd8d5c81663780b057c4b1ca3c9
Ubuntu <ubuntu@ip-172-31-40-85.us-east-2.compute.internal>
2018-08-11 22:32:50 +0000

    Fixed link to data folder

--- prepare-ami.sh
+++ prepare-ami.sh
@@ -3,7 +3,7 @@ cd data
 wget https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/vgg.zip
 unzip vgg.zip
 rm vgg.zip
-wget http://kitti.is.tue.mpg.de/kitti/data_road.zip
+wget https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/advanced_deep_learning/data_road.zip
 unzip data_road.zip
 rm data_road.zip
 pip install tqdm
